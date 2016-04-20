761db0385070fd8fb6af75950d0d8e29ae77e98d
Csaba Kállai <kallai.csaba@gmail.com>
2016-04-20 19:28:16 +0200

    Init Wait

---
+++ Asterix/Common/Wait.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Asterix.Framework.Common
+{
+    public static class Wait
+    {
+        public static void WaitUntilSuceed(Func<bool> condition, int timeOutInSecond = 60, int intervalInSecond = 1)
+        {
+            for (var i = 0; i < timeOutInSecond; i++)
+            {
+                if (condition())
+                {
+                    return;
+                } 
+        
+                Thread.Sleep(TimeSpan.FromSeconds(intervalInSecond));
+            }
+
+            throw new TimeoutException("Timeout reached without the condition begins true");
+        }
+
+    }
+}
