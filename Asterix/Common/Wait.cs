bddc381e2960e1f7e4e5130a0f2f98ef43825fae
Csaba Kállai <kallai.csaba@gmail.com>
2016-04-27 20:36:23 +0200

    Adding Common project with Wait feature. Adding NLog

--- Asterix/Common/Wait.cs
+++ Asterix/Common/Wait.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using System.Threading;
+using NLog;
 
 namespace Asterix.Framework.Common
 {
     public static class Wait
     {
-        public static void WaitUntilSuceed(Func<bool> condition, int timeOutInSecond = 60, int intervalInSecond = 1)
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public static void Until(Func<bool> condition, int timeOutInSecond = 60, int intervalInSecond = 1)
         {
+            logger.Info("Until was called");
             for (var i = 0; i < timeOutInSecond; i++)
             {
-                if (condition())
-                {
-                    return;
-                } 
-        
+                try
+                { 
+                    if (condition())
+                    {
+                        return;
+                    }
+                }
+                catch { }
+
                 Thread.Sleep(TimeSpan.FromSeconds(intervalInSecond));
             }
 
+            logger.Error("Timeout reached without the condition begins true");
             throw new TimeoutException("Timeout reached without the condition begins true");
         }
-
     }
+
+    //Legyen egy SafeWait osztály is, az nem dob exception
 }
