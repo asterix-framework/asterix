e1ea3cad63410562cbf9bfe576f26b1b8f576324
Csaba Kállai <kallai.csaba@gmail.com>
2016-05-22 17:02:15 +0200

    Add SafeWait class

---
+++ Asterix/Common/SafeWait.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Asterix.Framework.Common
+{
+    public static class SafeWait
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        public static void Until(Func<bool> condition, int timeOutInSecond = 60, int intervalInSecond = 1)
+        {
+            Logger.Info("Until was called");
+            WaitHelper.WaitForCoditionComesTrueOrTimeOutIsReached(condition, timeOutInSecond, intervalInSecond);
+        }
+    }
+}
+
